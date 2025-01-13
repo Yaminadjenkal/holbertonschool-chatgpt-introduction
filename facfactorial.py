81806a0a2b2aa9c722b387e44d6307c9ee3f4efd
Yaminadjenkal <mina20101951@hotmail.com>
2025-01-13 10:53:23 +0100

    fix my code

--- facfactorial.py
+++ facfactorial.py
@@ -8,6 +8,10 @@ def factorial(n):
 	n -= 1  # Décrémente n pour sortir de la boucle
 	return result
 
+# Vérification de l'argument passé et appel de la fonction
+	if len(sys.argv) > 1:
 	f = factorial(int(sys.argv[1]))
 print(f)
+	else:
+	print("Veuillez fournir un nombre en argument.")
 
