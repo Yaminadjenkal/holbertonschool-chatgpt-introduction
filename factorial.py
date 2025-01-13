db95848202a188dd102ad2822c53590cce1ac06d
Yaminadjenkal <mina20101951@hotmail.com>
2025-01-13 11:06:57 +0100

    fix my code

--- factorial.py
+++ factorial.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 import sys
-
 def factorial(n):
-	result = 1
-	while n > 1:
-		result *= n   # Cette ligne doit être indentée avec 4 espaces
-		n -= 1        # Cette ligne aussi doit être indentée avec 4 espaces
-	return result
-
-	if len(sys.argv) > 1:
-		f = factorial(int(sys.argv[1]))
-		print(f)
-	else:
-		print("Veuillez fournir un nombre en argument.")
-
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1
+    return result
+if len(sys.argv) != 2:
+    print("Usage: python3 script.py <number>")
+    sys.exit(1)
+f = factorial(int(sys.argv[1]))
+print(f)
