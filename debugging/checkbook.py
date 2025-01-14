b34e696271957696789ad378eac59a8dbe5e9fd3
Yaminadjenkal <mina20101951@hotmail.com>
2025-01-14 11:06:46 +0100

    fix my code

---
+++ debugging/checkbook.py
@@ -0,0 +1,3 @@
+This script simulates a checkbook where a user can deposit money, withdraw money, and check their balance. The program includes error handling for invalid commands and inputs to prevent it from crashing.
+
+
